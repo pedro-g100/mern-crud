a651a4f8d126e0ea6df99e990bbe225ebddeb33e
pedro-g100 <pedrog.100@hotmail.com>
2025-06-24 01:10:40 -0300

    conectando mongodb com expressjs

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+
+const app = express();
+app.use(bodyParser.json());
+dotenv.config();
+
+const PORT = process.env.PORT || 7000;
+const MONGOURL = process.env.MONGO_URL;
+
+mongoose
+  .connect(MONGOURL)
+  .then(() => {
+    console.log("Conectado ao MongoDB com sucesso!");
+    app.listen(PORT, () => {
+      console.log(`Servidor rodando na porta ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error(error);
+  });
