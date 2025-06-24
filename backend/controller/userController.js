2ad2e9d1f8f94dc4cc1712f06045857f131235a4
pedro-g100 <pedrog.100@hotmail.com>
2025-06-24 01:50:18 -0300

    get usuário pelo id

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -26,4 +26,17 @@ export const getAllUsers = async (req, res) => {
     } catch (error) {
         res.status(500).json({ errorMessage: error.message });
     }
-}
+};
+
+export const getUserById = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const userExist = await User.findById(id);
+        if (!userExist) {
+            return res.status(404).json({ message: "Usuário não encontrado!" });
+        }
+        res.status(200).json(userExist)
+    } catch (error) {
+        res.status(500).json({ errorMessage: error.message });
+    }
+};
